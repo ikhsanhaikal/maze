3738d43a76f4e7e48047b6be986786c6c89aa8e3
ikhsan haikal <andvelop4@gmail.com>
2020-03-27 11:04:29 +0700

    backtraking finally works but still hardcode for the map of maze

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 let G = [
-  [0, 's', 0, 0, 0, 0],
-  [0, 1, 1, 0, 1, 0],
-  [0, 0, 1, 0, 1, 1],
-  ['e', 0, 1, 1, 1, 0],
-  [0, 1, 1, 0, 0, 0]
+  [0, 0, 0, 1, 0, 0],
+  [1, 1, 1, 0, 1, 0],
+  [0, 0, 1, 0, 1, 0],
+  ['e', 0, 1, 1, 1, 1],
+  [1, 0, 1, 0, 0, 's'],
+  [1, 1, 1, 1, 0, 0]
 ]
+
+//check if neighbour # don't add to queue just
+//set visited to true and change the color to red
 
 const container = document.createElement('div');
 const button    = document.querySelector('.start');
@@ -23,12 +27,17 @@ for (let row of G) {
     cell.id = `${i}-${j}`;
     if (node == 1)
       cell.textContent = '.';
-    else if (node == 's')
+    else if (node == 's') {
       cell.textContent = 'S';
+      cell.style.background = 'blue';
+      cell.style.color = 'white';
+    }
     else if (node == 'e')
       cell.textContent = 'E';
-    else
+    else {
       cell.textContent = '#';
+      cell.style.background = 'black';
+    }
     container.appendChild(cell);
     j++;
   }
@@ -38,7 +47,7 @@ for (let row of G) {
 
 let queue = [];
 let visited = [];
-let obj = {}
+let coord;
 for (let i = 0; i < G.length; i++) {
   let arr = [];
   for (let j = 0; j < G[0].length; j++) {
@@ -51,7 +60,7 @@ for (let i = 0; i < G.length; i++) {
   }
   visited.push(arr);
 }
-const start = visited[0][1];
+const start = visited[4][5];
 start.visited = true;
 queue.push(start);
 //------------------------------------------------------------------------------
@@ -62,53 +71,78 @@ button.addEventListener('click', ()=> {
     if (queue.length){
       let u = queue.shift();
       let c = document.getElementById(`${u.row}-${u.nth}`);
-
-
-      if (c.textContent === '#') {
-        // c.style.backgroundColor = 'salmon';
-        // c.style.color = 'white';
-        c.classList.toggle('on');
+      if (c.textContent !== '#' && c.textContent !== 'S') {
+        c.style.backgroundColor = '#FFFF66';
       }
-      else
-        c.style.backgroundColor = 'lightyellow';
+      if (c.textContent === 'E') {
+        coord = [u.row, u.nth];
+      }
       //up
       console.log(u);
       if (u.row - 1 >= 0) {
-        if (visited[u.row][u.nth].visited == null) {
+        if (visited[u.row-1][u.nth].visited == null) {
           let v = visited[u.row - 1][u.nth];
-          v.predecessor = [u.row, u.nth];
-          v.visited = true;
-          queue.push(v);
+          let n = document.getElementById(`${u.row - 1}-${u.nth}`);
+          if (n.textContent !== '#') {
+            v.predecessor = [u.row, u.nth];
+            v.visited = true;
+            queue.push(v);
+          }
         }
       }
       //right
       if (u.nth + 1 < G[0].length) {
         if (visited[u.row][u.nth+1].visited == null) {
           let v = visited[u.row][u.nth + 1];
-          v.predecessor = [u.row, u.nth];
-          v.visited = true;
-          queue.push(v);
+          let n = document.getElementById(`${u.row}-${u.nth + 1}`);
+          if (n.textContent !== '#') {
+            v.predecessor = [u.row, u.nth];
+            v.visited = true;
+            queue.push(v);
+          }
         }
       }
       //down
       if (u.row + 1 < G.length) {
         if (visited[u.row+1][u.nth].visited == null) {
           let v = visited[u.row + 1][u.nth];
-          v.predecessor = [u.row, u.nth];
-          v.visited = true;
-          queue.push(v);
+          let n = document.getElementById(`${u.row + 1}-${u.nth}`);
+          if (n.textContent !== '#') {
+            v.predecessor = [u.row, u.nth];
+            v.visited = true;
+            queue.push(v);
+          }
         }
        }
       //left
        if (u.nth - 1 >= 0) {
          if (visited[u.row][u.nth - 1].visited == null){
            let v = visited[u.row][u.nth - 1];
-           v.predecessor = [u.row, u.nth];
-           v.visited = true;
-           queue.push(v);
+           let n = document.getElementById(`${u.row}-${u.nth - 1}`);
+           if (n.textContent !== '#') {
+             v.predecessor = [u.row, u.nth];
+             v.visited = true;
+             queue.push(v);
+           }
          }
        }
-    } else {clearInterval(timerId);}
+    } else {
+      let x = coord[0];
+      let y = coord[1];
+      console.log('before'+ x + ' ' + y);
+      let timeout = setInterval(function() {
+        if(visited[x][y].predecessor !== null) {
+          const b = document.getElementById(`${x}-${y}`);
+          console.log(b);
+          coord = visited[x][y].predecessor;
+          b.style.backgroundColor = 'lightgreen';
+          x = coord[0];
+          y = coord[1];
+        } else { clearInterval(timeout); }
+      }, 500);
+
+      clearInterval(timerId);
+    }
   }, 1000);
 
 });
